2b913c45b2881b089da39701ded340599ce2766f
wikiedu <eduhand92@gmail.com>
2016-11-21 16:03:47 +0100

    landing page changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to 'login'
+      redirect_to login_path
     end
   end
 
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   #   redirect_to '/'
   # end
 
-  def destroy
+def destroy
   session[:user_id] = nil
   redirect_to '/'
 end
